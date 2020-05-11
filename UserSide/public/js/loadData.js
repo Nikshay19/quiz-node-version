74018be6757fa972baba884b4ac2aa189ca2f3c6
Nikshay <38729484+Nikshay19@users.noreply.github.com>
2020-05-11 12:11:47 +0530

    added timeout function for loading data to make sure that token is set

--- UserSide/public/js/loadData.js
+++ UserSide/public/js/loadData.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    const token = localStorage.getItem('access-token');
-    $.ajax({
-        type: "GET",
-        url: "http://localhost:4000/loadData?token=" + token + "",
-        dataType: "JSON",
-        success: function(response) {
-            $('.dataloadcontainer').html(response.output)
-        }
-    });
+    setTimeout(function() {
+        const token = localStorage.getItem('access-token');
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:4000/loadData?token=" + token + "",
+            dataType: "JSON",
+            success: function(response) {
+                $('.dataloadcontainer').html(response.output)
+            }
+        });
+    }, 2000);
 });
