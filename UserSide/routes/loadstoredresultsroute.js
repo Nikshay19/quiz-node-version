a7e4aa18cb5a37b46aa69b66ff4cb495be319cff
Nikshay <38729484+Nikshay19@users.noreply.github.com>
2020-05-14 22:16:04 +0530

    finished setting up the stored results page

--- UserSide/routes/loadstoredresultsroute.js
+++ UserSide/routes/loadstoredresultsroute.js
@@ -5,13 +5,38 @@ const token_middleware = require('../middlewares/jwtauth');
 
 router.get('/', token_middleware, (req, res) => {
     if (req.status === "exists") {
+        var htmloutput = ' ';
+        var obj;
         const { user } = req.getuseremail;
         const select_query = 'select * from savedresults where user = "' + user + '"';
         dbcon.query(select_query, (err, result) => {
+            obj = result;
             if (err) {
                 console.log(err)
             } else {
-                console.log(result[0].userwronganswers.split(','))
+                for (let i = 0; i < result.length; i++) {
+                    const userdata = result[i].userwronganswers.split(',');
+                    if (userdata[i] !== undefined) {
+                        htmloutput += '<div class="gchart" id="piechart' + i + '" style="width: 500px; height: 500px; margin-left: 285px;"></div>' +
+                            '<div class="gchart1" id="piechartforcorrect' + i + '" style="width: 500px; height: 500px; margin-left: 713px; margin-top: -489px;"></div>' +
+                            '<div class="card">' +
+                            '<div class="card-body">' +
+                            '<ul class="list-group">';
+                        for (let j = 0; j < userdata.length; j++) {
+                            htmloutput += '<li class="list-group-item">' + userdata[j] + '</li>'
+                        }
+                        htmloutput += '</ul>' +
+                            '</div>' +
+                            '</div>' +
+                            '<br>';
+                    } else {
+                        delete obj[i];
+                    }
+                }
+                res.json({
+                    output: htmloutput,
+                    data: obj
+                })
             }
         })
     }
